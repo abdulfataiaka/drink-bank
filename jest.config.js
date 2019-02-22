7895fc6a8e70ad9a98fe61d23086fdaafe72bcc0
abdulfataiaka <abdulfataiaka@gmail.com>
2019-02-22 01:59:55 +0100

    completed setup for jest

---
+++ jest.config.js
@@ -0,0 +1,67 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
+
+  globals: {},
+
+  moduleDirectories: [
+    "node_modules"
+  ],
+
+  moduleFileExtensions: [
+    "js",
+    "jsx"
+  ],
+
+  // A map from regular expressions to module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/specs/support/mocks/styleMock.js"
+  },
+
+  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  modulePathIgnorePatterns: [],
+
+  roots: [
+    "<rootDir>",
+    "<rootDir>/specs"
+  ],
+
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: [
+    "<rootDir>/specs/support/enzyme.js"
+  ],
+
+  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // snapshotSerializers: [],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/specs/**/*.spec.js?(x)"
+  ],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: [
+    "/node_modules/"
+  ],
+};
---
+++ specs/demo.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../src/components/App';
+
+describe('App Component', () => {
+  it('should render component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.html()).toContain('Welcome');
+  });
+});
